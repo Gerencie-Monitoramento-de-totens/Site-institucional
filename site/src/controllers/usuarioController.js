432f3137a3e416bfce4af4ca1ffab3ff86b84f35
EuRossi <gabriel.rossi@sptech.school>
2022-11-30 02:42:19 -0300

    Mostrando usuarios por empresa

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -8,7 +8,8 @@ function testar(req, res) {
 }
 
 function listar(req, res) {
-    usuarioModel.listar()
+    var idEmpresa = req.params.idEmpresa;
+    usuarioModel.listar(idEmpresa)
         .then(function (resultado) {
             if (resultado.length > 0) {
                 res.status(200).json(resultado);
